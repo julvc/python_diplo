dc76092d392ca43e20644434877eae3047fa0603
Julio Varas C <jvarascontreras@gmail.com>
2024-07-03 22:37:19 -0400

    Se avanza con clase personas

--- MP3/personas.py
+++ MP3/personas.py
@@ -66,12 +66,38 @@ class Cocinero(Persona):
 
 ### INICIO PARTE 2.4 ###
 class Cliente(Persona):
-    def __init__(self, nombre,platos_preferidos):
+    def __init__(self,nombre,platos_preferidos):
         super().__init__(nombre)
         self.platos_preferidos = platos_preferidos
-        
+
+
+#recibir_pedido(pedido, demora): Recibe como argumento el pedido, que es
+#una lista de objetos de la clase Bebestible o Comestible, y la demora, que es
+#un int que indica cuánto se demoró la entrega de los platos. 
+# Primero se debe definir una calificación que comienza en 10. 
+# Si la cantidad de platos en el pedido es menor a la cantidad de platos_preferidos del cliente o 
+# si la demora es mayor o igual a 20, la calificación es dividida a la mitad. 
+# Luego, por cada plato, el cliente cambiará su calificación dependiendo de la calidad del plato.
+#Si la calidad del plato es mayor o igual a 11, a la calificación se le suma 1.5.
+#Si la calidad es menor o igual a 8, la calificación disminuye en 3, no pudiendo
+#este valor descender de 0. En cualquier otro caso la calificación se mantiene.
+#Finalmente se debe retornar la calificación que el cliente le ha asignado al
+#restaurante   
     def recibir_pedido(self, pedido, demora):
-        pass        
+        calificacion = 10
+        if pedido < len(self.platos_preferidos) or demora >= 20:
+            calificacion = 5
+        
+        for i in range(len(pedido)):
+            plato = pedido[i]
+            if plato.calidad >= 11:
+                calificacion += 1.5
+            if plato.calidad <= 8:
+                calificacion -= 3
+            if calificacion < 0:
+                calificacion = 0
+            
+        return calificacion        
 ### FIN PARTE 2.4 ###
 
 
@@ -90,8 +116,9 @@ if __name__ == "__main__":
         print(f"El cocinero {un_cocinero.nombre} tiene una habilidad: {un_cocinero.habilidad}")
         print(f"El repatidor {un_repartidor.nombre} tiene una tiempo de entrega: {un_repartidor.tiempo_entrega} seg")
         print(f"El cliente {un_cliente.nombre} tiene los siguientes platos favoritos:")
-        for plato in un_cliente.platos_preferidos.values():
-            print(f" - {plato[1]}: {plato[0]}")
+        for plato in un_cliente.platos_preferidos:
+            print(plato)
+            #print(f" - {plato[1]}: {plato[0]}")
     except TypeError:
         print("Hay una cantidad incorrecta de argumentos en algún inicializador y/o todavía no defines una clase")
     except AttributeError:
